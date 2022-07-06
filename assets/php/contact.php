011b8d71c4ec31fe4b3707126498c4c2ef1c37d4
Tashen Dinal <80526793+tashendinal@users.noreply.github.com>
2022-07-06 09:45:52 +0530

    Add files via upload

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'Demo contact form <demo@domain.com>';
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'Demo contact form <tashendinal.developer@gmail.com>';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Message sent successfully. Thank you, will get back to you soon!';
